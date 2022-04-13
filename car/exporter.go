5810c962bace7b7cdffbb4f816eb3d61b6aa6d3a
Sudeep <sudeepdino008@gmail.com>
2022-04-13 09:14:32 +0530

    added content archive (car) exporter

---
+++ car/exporter.go
@@ -0,0 +1,18 @@
+// facility to create car files out of unixfs nodes
+
+package car
+
+import (
+	"context"
+	"io"
+
+	"github.com/ipfs/go-cid"
+)
+
+type CarExporter interface {
+	export(ctx context.Context, contentRoot cid.Cid, writer io.Writer) error
+
+	// export car file to multiple destinations.
+	// failure in one writer doesn't fail write attempts to other writers
+	multiExport(ctx context.Context, contentRoot cid.Cid, writers []io.Writer) error
+}
---
+++ car/gocar.go
@@ -0,0 +1,52 @@
+// A car exporter based on go-car module
+package car
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/ipfs/go-cid"
+	coreiface "github.com/ipfs/interface-go-ipfs-core"
+	gocar "github.com/ipld/go-car"
+	selectorparse "github.com/ipld/go-ipld-prime/traversal/selector/parse"
+)
+
+// implements the CarExporter
+type carExporter struct {
+	api coreiface.CoreAPI
+}
+
+func (exp *carExporter) export(ctx context.Context, contentRoot cid.Cid, writer io.Writer) error {
+	store := dagStore{dag: exp.api.Dag(), ctx: ctx}
+	dag := gocar.Dag{Root: contentRoot, Selector: selectorparse.CommonSelector_ExploreAllRecursively}
+	// TraverseLinksOnlyOnce is safe for an exhaustive selector but won't be when we allow
+	// arbitrary selectors here
+	scar := gocar.NewSelectiveCar(ctx, store, []gocar.Dag{dag}, gocar.TraverseLinksOnlyOnce())
+	return scar.Write(writer)
+}
+
+func (exp *carExporter) multiExport(ctx context.Context, contentRoot cid.Cid, writers []io.Writer) error {
+	if len(writers) == 0 {
+		return fmt.Errorf("no writers provided")
+	}
+
+	if len(writers) == 1 {
+		return exp.export(ctx, contentRoot, writers[0])
+	}
+
+	store := dagStore{dag: exp.api.Dag(), ctx: ctx}
+	dag := gocar.Dag{Root: contentRoot, Selector: selectorparse.CommonSelector_ExploreAllRecursively}
+	scar := gocar.NewSelectiveCar(ctx, store, []gocar.Dag{dag}, gocar.TraverseLinksOnlyOnce())
+
+	preparedScar, err := scar.Prepare()
+	if err != nil {
+		return err
+	}
+
+	for _, writer := range writers {
+		preparedScar.Dump(ctx, writer)
+	}
+
+	return nil
+}
