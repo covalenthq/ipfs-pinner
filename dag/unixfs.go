c76d8825a744793125511622310ba676b08ddc99
Sudeep <sudeepdino008@gmail.com>
2022-04-13 23:52:23 +0530

    expose filepath rather than Ipfs file node as input in DagGenerate interface

--- dag/unixfs.go
+++ dag/unixfs.go
@@ -2,6 +2,7 @@ package dag
 
 import (
 	"context"
+	"os"
 
 	"github.com/ipfs/go-cid"
 	files "github.com/ipfs/go-ipfs-files"
@@ -22,10 +23,27 @@ func NewUnixfsAPI(ipfs coreiface.CoreAPI, cidVersion int, cidGenerationOnly bool
 	return &api
 }
 
-func (api *unixfsApi) GenerateDag(ctx context.Context, node files.Node) (cid.Cid, error) {
+func (api *unixfsApi) GenerateDag(ctx context.Context, filepath string) (cid.Cid, error) {
+	node, err := getNodeFor(filepath)
+	if err != nil {
+		return cid.Undef, err
+	}
 	rpath, err := api.ipfs.Unixfs().Add(ctx, node, api.addOptions...)
 	if err != nil {
 		return cid.Undef, err
 	}
 	return rpath.Cid(), nil
+}
+
+func getNodeFor(filepath string) (files.Node, error) {
+	stat, err := os.Stat(filepath)
+	if err != nil {
+		return nil, err
+	}
+	n, err := files.NewSerialFile(filepath, true, stat)
+	if err != nil {
+		return nil, err
+	}
+
+	return n, nil
 }
