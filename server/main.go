51c2f33deb154836a1d489eb228973c79553bdb0
fatemebagherii <fa.bagheri.a@gmail.com>
2022-10-04 14:48:16 -0700

    add cli arguments

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
@@ -17,19 +18,25 @@ var UPLOAD_FILE = "temp.txt"
 
 // run this: go run main.go "3000"
 func main() {
-	port_number := os.Args[1]
+
+	port_number := flag.String("port", "3000", "port number for the server")
+	UPLOAD_FILE := flag.String("file", "temp.txt", "the default address of the file to be pinned")
+	token := flag.String("jwt", "", "jwt token for web3.storage")
+	// port_number := os.Args[1]
 
 	println(port_number)
 
-	setUpAndRunServer(port_number)
+	setUpAndRunServer(*port_number, *UPLOAD_FILE, *token)
 }
 
-func setUpAndRunServer(port_number string) {
+func setUpAndRunServer(port_number string, UPLOAD_FILE string, token string) {
 	mux := http.NewServeMux()
-
-	token, present := os.LookupEnv(WEB3_JWT)
-	if !present {
-		log.Fatalf("token (%s) not found in env", WEB3_JWT)
+	if token == "" {
+		var present bool
+		token, present = os.LookupEnv(WEB3_JWT)
+		if !present {
+			log.Fatalf("token (%s) not found in env", WEB3_JWT)
+		}
 	}
 
 	clientCreateReq := client.NewClientRequest(core.Web3Storage).BearerToken(token)
