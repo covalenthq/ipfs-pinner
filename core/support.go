88f6647061de0f7e0b8a3679c92234dd5f8f0e6f
sudeep <sudeepdino008@gmail.com>
2024-01-04 23:05:18 +0530

    update ipfs-pinnern version to 0.1.17

--- core/support.go
+++ core/support.go
@@ -17,7 +17,7 @@ const (
 	// IpfsPinnerVersionMinor is Minor version component of the current release
 	IpfsPinnerVersionMinor = 1
 	// IpfsPinnerVersionPatch is Patch version component of the current release
-	IpfsPinnerVersionPatch = 16
+	IpfsPinnerVersionPatch = 17
 	clientIdentifier       = "ipfs-pinner" // Client identifier to advertise over the network
 )
 
